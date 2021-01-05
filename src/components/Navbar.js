146fe939c5dc877f9d809112602b3d1975e20c3d
joyAct <joylee.dev@gmail.com>
2021-01-05 17:36:54 +0900

    Add the icon on mobile

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,10 +3,12 @@ import styled, { css } from 'styled-components';
 import { Link } from 'react-router-dom';
 import { menuData } from '../data/MenuData';
 import { Button } from './Button';
+import Bars from '../images/bars.svg';
 
 const Nav = styled.nav`
   display: flex;
   justify-content: space-between;
+  align-items: center;
   width: 100%;
   height: 60px;
   padding: 1rem 2rem;
@@ -30,7 +32,18 @@ const Logo = styled(Link)`
   font-style: italic;
 `;
 
-const MenuBars = styled.i``;
+const MenuBars = styled.i`
+  display: none;
+
+  @media screen and (max-width: 768px) {
+    display: block;
+    background-image: url(${Bars});
+    background-size: contain;
+    width: 40px;
+    height: 40px;
+    cursor: pointer;
+  }
+`;
 
 const NavMenu = styled.div`
   display: flex;
