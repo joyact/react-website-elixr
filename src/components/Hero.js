c7d21a7fcd9b63d23ff612a18fb3554cab0ace0c
Joy Lee <joylee.dev@gmail.com>
2021-01-18 21:28:46 +0900

    The style of the main page

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 import { Button } from './Button';
 import { IoMdArrowRoundForward } from 'react-icons/io';
 import { IoArrowForward, IoArrowBack } from 'react-icons/io5';
@@ -21,14 +21,111 @@ const HeroWrapper = styled.div`
   overflow: hidden;
 `;
 
-const HeroSlider = styled.div``;
-const HeroSliderInner = styled.div``;
-const HeroImage = styled.img``;
-const HeroContent = styled.div``;
-const Arrow = styled(IoMdArrowRoundForward)``;
-const SliderButtons = styled.div``;
-const PrevArrow = styled(IoArrowForward)``;
-const NextArrow = styled(IoArrowBack)``;
+const HeroSlider = styled.div`
+  width: 100%;
+  height: 100%;
+  z-index: 1;
+`;
+
+const HeroSliderInner = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  &::before {
+    content: '';
+    position: absolute;
+    z-index: 2;
+    width: 100%;
+    height: 100%;
+    bottom: 0vh;
+    left: 0;
+    overflow: hidden;
+    opacity: 0.4;
+    background: linear-gradient(
+      0deg,
+      rgba(0, 0, 0, 0.2) 0%,
+      rgba(0, 0, 0, 0.2) 50%,
+      rgba(0, 0, 0, 0.6) 100%
+    );
+  }
+`;
+
+const HeroImage = styled.img`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100vw;
+  height: 100vh;
+  object-fit: cover;
+`;
+
+const HeroContent = styled.div`
+  position: relative;
+  z-index: 10;
+  display: flex;
+  flex-direction: column;
+  max-width: 1600px;
+  width: calc(100% - 100px);
+  color: #fff;
+
+  h1 {
+    font-size: clamp(1rem, 8vw, 2rem);
+    font-weight: 400;
+    margin-bottom: 0.8rem;
+    text-shadow: 0px 0px 20px rgba(0, 0, 0, 0.4);
+    text-transform: uppercase;
+    text-align: left;
+  }
+
+  p {
+    margin-bottom: 1.2rem;
+    text-shadow: 0px 0px 20px rgba(0, 0, 0, 0.4);
+  }
+`;
+
+const Arrow = styled(IoMdArrowRoundForward)`
+  margin-left: 0.5rem;
+  margin-top: 0.2rem;
+`;
+
+const SliderButtons = styled.div`
+  position: absolute;
+  right: 50px;
+  bottom: 50px;
+  display: flex;
+  z-index: 10;
+`;
+
+const arrowButtons = css`
+  width: 50px;
+  height: 50px;
+  color: #fff;
+  cursor: pointer;
+  background: #000d1a;
+  border-radius: 50%;
+  padding: 10px;
+  margin-right: 1rem;
+  transition: 0.3s;
+
+  &:hover {
+    background: #cd853f;
+    transform: scale(1.05);
+  }
+`;
+
+const PrevArrow = styled(IoArrowBack)`
+  ${arrowButtons}
+`;
+
+const NextArrow = styled(IoArrowForward)`
+  ${arrowButtons}
+`;
 
 function Hero({ slides }) {
   return (
@@ -37,7 +134,7 @@ function Hero({ slides }) {
         {slides.map((slide, index) => (
           <HeroSlider key={index}>
             <HeroSliderInner>
-              <HeroImage />
+              <HeroImage src={slide.image} alt={slide.alt} />
               <HeroContent>
                 <h1>{slide.title}</h1>
                 <p>{slide.price}</p>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -12,7 +12,6 @@ const Nav = styled.nav`
   width: 100%;
   height: 60px;
   padding: 1rem 2rem;
-  background-color: #000;
   z-index: 100;
   position: fixed;
 `;
