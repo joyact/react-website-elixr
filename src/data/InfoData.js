7a41c11534a69da6478854f801a8d2c9e90e9422
Joy Lee <joylee.dev@gmail.com>
2021-01-20 23:31:40 +0900

    Add data for info section

---
+++ src/data/InfoData.js
@@ -0,0 +1,26 @@
+import ImageOne from '../images/interior-1.jpg';
+import ImageTwo from '../images/interior-2.jpg';
+
+export const InfoDataOne = {
+  heading: 'Explore our beautiful homes',
+  paragraphOne:
+    'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam ac mi eu nulla pulvinar pellentesque et eget libero. ',
+  paragraphTwo:
+    'Nunc accumsan tempor pretium. Ut feugiat libero sit amet tempus facilisis. Sed at hendrerit lectus. Donec vehicula dignissim mi, ac volutpat augue accumsan quis. Pellentesque at iaculis felis.',
+  buttomLabel: 'View Homes',
+  image: ImageOne,
+  reverse: false,
+  delay: 100,
+};
+
+export const InfoDataTwo = {
+  heading: 'Modern Designs',
+  paragraphOne:
+    'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam ac mi eu nulla pulvinar pellentesque et eget libero. ',
+  paragraphTwo:
+    'Nunc accumsan tempor pretium. Ut feugiat libero sit amet tempus facilisis. Sed at hendrerit lectus. Donec vehicula dignissim mi, ac volutpat augue accumsan quis. Pellentesque at iaculis felis.',
+  buttomLabel: 'View Homes',
+  image: ImageTwo,
+  reverse: true,
+  delay: 300,
+};
